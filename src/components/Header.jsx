434f9266ca001bb819f5b1a1d273769a35755580
Figby222 <figby222@outlook.com>
2024-05-10 05:21:37 -0700

    Add header & h1 to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 function Header() {
     return (
-        {
-            <>
-            
-        }
+        <>
+        <header>
+            <h1>CV-Application</h1>
+        </header>
+        </>
     )
 }
+
+export default Header;
