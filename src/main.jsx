68c83f44ceaa526e759f1bb9f3fc7563a8298eab
Figby222 <figby222@outlook.com>
2024-05-10 06:00:55 -0700

    Import and display Main

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header.jsx';
+import Main from './components/Main.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Main />
   </React.StrictMode>,
 )
