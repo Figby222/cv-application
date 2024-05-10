1086b2b209bad9ad520e4af6d44a92c14c4b9719
Figby222 <figby222@outlook.com>
2024-05-10 05:36:51 -0700

    Import Dialog

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import Dialog from './utility-components/Dialog';
+
+function Main() {
+    return (
+        <>
+            <main>
+               <Dialog open={true}>
+                    <p>hi</p>
+                </Dialog> 
+            </main>
+        </>
+    )
+}
+
+export default Main;
