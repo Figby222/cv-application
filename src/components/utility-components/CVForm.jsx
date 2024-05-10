8c6d90ec9a514547fa838607efc3224ccf56f92f
Figby222 <figby222@outlook.com>
2024-05-10 06:00:08 -0700

    Add First Name and Last Name fields

--- src/components/utility-components/CVForm.jsx
+++ src/components/utility-components/CVForm.jsx
@@ -5,6 +5,28 @@ function CVForm({
         <>
             <form method="post">
                 <h2>{heading}</h2>
+                <fieldset className="contact-info">
+                    <label>
+                        First Name:&nbsp;
+                        <input
+                            type="text"
+                            id="first-name"
+                            name="first-name"
+                            placeholder="John"
+                        >
+                        </input>
+                    </label>
+                    <label>
+                        Last Name:&nbsp;
+                        <input
+                            type="text"
+                            id="last-name"
+                            name="last-name"
+                            placeholder="Doe"
+                        >
+                        </input>
+                    </label>
+                </fieldset>
             </form>
         </>
     )
