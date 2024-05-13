eec049a8a8af9326b6f7a2ba9ebbaa723515adcc
Figby222 <figby222@outlook.com>
2024-05-12 17:52:43 -0700

    Extract contact info into ContactInfo.jsx
    Import ContactInfo.jsx

--- src/components/utility-components/CVForm/CVForm.jsx
+++ src/components/utility-components/CVForm/CVForm.jsx
@@ -1,54 +1,17 @@
 import { useState } from 'react';
 import Education from './Education.jsx';
+import ContactInfo from './ContactInfo.jsx';
 
 
 function CVForm({
     heading,
 }) {
-    const [ firstName, setFirstName ] = useState("");
-    const [ lastName, setLastName ] = useState("");
-    
-
-    function handleFirstNameChange(e) {
-        setFirstName(e.target.value);
-    }
-
-    function handleLastNameChange(e) {
-        setLastName(e.target.value);
-    }
-
-    
-
     return (
         <>
             <form method="post">
                 <h2>{heading}</h2>
                 <fieldset className="contact-info">
-                    <h3>Contact Info</h3>
-                    <label>
-                        First Name:&nbsp;
-                        <input
-                            type="text"
-                            id="first-name"
-                            name={"first-name"}
-                            placeholder="John"
-                            value={firstName}
-                            onChange={handleFirstNameChange}
-                        >
-                        </input>
-                    </label>
-                    <label>
-                        Last Name:&nbsp;
-                        <input
-                            type="text"
-                            id="last-name"
-                            name="last-name"
-                            placeholder="Doe"
-                            value={lastName}
-                            onChange={handleLastNameChange}
-                        >
-                        </input>
-                    </label>
+                    <ContactInfo />
                 </fieldset>
                 <fieldset>
                     <Education />
