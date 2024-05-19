10bd7d77fda9d87b2a1d68cab526123677acebfc
Figby222 <figby222@outlook.com>
2024-05-18 23:40:57 -0700

    Add .school to returned div

--- src/components/utility-components/CVForm/School.jsx
+++ src/components/utility-components/CVForm/School.jsx
@@ -6,7 +6,7 @@ function School({
     handleSchoolChange
 }) {
     return (
-        <div>
+        <div className="school">
             <label htmlFor="name">Name: </label>
             <input 
                 type="text"
