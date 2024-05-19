baec438ef472eefd0a68ffe2be40fcf59acf9111
Figby222 <figby222@outlook.com>
2024-05-18 22:25:26 -0700

    Move editing state from CVForm to Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
 import CVForm from './utility-components/CVForm/CVForm.jsx';
+import { useState } from 'react';
 
 function Main() {
+    const [ editing, setEditing ] = useState(true);
     return (
         <>
             <main>
--- src/components/utility-components/CVForm/CVForm.jsx
+++ src/components/utility-components/CVForm/CVForm.jsx
@@ -7,7 +7,6 @@ function CVForm({
     heading,
     initialSchools
 }) {
-    const [ editing, setEditing ] = useState(true);
 
     return (
         <>
