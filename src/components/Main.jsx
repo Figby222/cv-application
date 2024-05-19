c4efc3267e374d5974987980ec1bfa9479ff7177
Figby222 <figby222@outlook.com>
2024-05-18 22:26:57 -0700

    Add application object

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,16 @@
 import CVForm from './utility-components/CVForm/CVForm.jsx';
 import { useState } from 'react';
 
+const application = {
+    contactInfo: {
+        firstName: "",
+        lastName: "",
+        email: "",
+    },
+    schools: [],
+    jobs: []
+}
+
 function Main() {
     const [ editing, setEditing ] = useState(true);
     return (
