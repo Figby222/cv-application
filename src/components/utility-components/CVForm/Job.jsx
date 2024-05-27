a8e0aa8bb1cc17f857e4ea41b836b7e3a34ddace
Figby222 <figby222@outlook.com>
2024-05-27 15:04:36 -0700

    Add job state & use it in handleJobChange()
    import useState from react

--- src/components/utility-components/CVForm/Job.jsx
+++ src/components/utility-components/CVForm/Job.jsx
@@ -1,12 +1,17 @@
 import Input from '../Input.jsx';
+import { useState } from 'react';
 
 function Job({
     jobObject,
 }) {
+
+    const [ job, setJob ] = useState(jobObject);
     function handleJobChange(property, value) {
-        const newJobObject = {...jobObject}
-        newJobObject[property] = value;
-        jobObject = newJobObject;
+        const newJob = {...job}
+        newJob[property] = value;
+
+        jobObject[property] = value;
+        setJob(newJob)
     }
     return (
         <>
