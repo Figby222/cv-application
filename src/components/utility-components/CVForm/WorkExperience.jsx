a5cfc6f9bb959321cc42f2aa05158803aa706861
Figby222 <figby222@outlook.com>
2024-05-27 14:32:25 -0700

    Add jobIdCounter state
    import useState from react

--- src/components/utility-components/CVForm/WorkExperience.jsx
+++ src/components/utility-components/CVForm/WorkExperience.jsx
@@ -1,7 +1,9 @@
 import Job from './Job.jsx';
+import { useState } from 'react';
 function WorkExperience({
     jobsDataObject,
 }) {
+    const [ jobIdCounter, setJobIdCounter ] = useState(jobsDataObject.jobIdCounter);
     return (
         <>
         <h3>Work Experience</h3>
