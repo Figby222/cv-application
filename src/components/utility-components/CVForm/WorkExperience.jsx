45b8f47320ce3b95a502d3f490479e26e71ef3c4
Figby222 <figby222@outlook.com>
2024-05-27 14:39:53 -0700

    Add jobIds state

--- src/components/utility-components/CVForm/WorkExperience.jsx
+++ src/components/utility-components/CVForm/WorkExperience.jsx
@@ -4,6 +4,11 @@ function WorkExperience({
     jobsDataObject,
 }) {
     const [ jobIdCounter, setJobIdCounter ] = useState(jobsDataObject.jobIdCounter);
+    const [ jobIds, setJobIds ] = useState(
+        jobsDataObject.jobs.map(job => job.id)
+    )
+
+    
     return (
         <>
         <h3>Work Experience</h3>
