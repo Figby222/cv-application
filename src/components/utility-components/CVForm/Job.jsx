7f203904d666e32bf7d5f9179fde3d41a1f483a8
Figby222 <figby222@outlook.com>
2024-05-27 15:00:42 -0700

    Add function handleJobChange()

--- src/components/utility-components/CVForm/Job.jsx
+++ src/components/utility-components/CVForm/Job.jsx
@@ -1,6 +1,13 @@
+import Input from '../Input.jsx';
+
 function Job({
     jobObject,
 }) {
+    function handleJobChange(property, value) {
+        const newJobObject = {...jobObject}
+        newJobObject[property] = value;
+        jobObject = newJobObject;
+    }
     return (
         <>
         </>
