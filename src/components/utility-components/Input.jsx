a395d17d023b4a9517e75716a7c41c6871565894
Figby222 <figby222@outlook.com>
2024-05-27 18:52:31 -0700

    Wrap Inputs in div.Input

--- src/components/utility-components/Input.jsx
+++ src/components/utility-components/Input.jsx
@@ -9,15 +9,17 @@ function Input({
 }) {
         return (
             <>
-                <label htmlFor={id}>{label}</label>
-                <input 
-                    type={type}
-                    id={id}
-                    name={name}
-                    placeholder={placeholder}
-                    value={value ? value : ""}
-                    onChange={onChange}
-                />
+                <div className="Input">
+                    <label htmlFor={id}>{label}</label>
+                    <input
+                        type={type}
+                        id={id}
+                        name={name}
+                        placeholder={placeholder}
+                        value={value ? value : ""}
+                        onChange={onChange}
+                    />
+                </div>
             </>
         )
 }
